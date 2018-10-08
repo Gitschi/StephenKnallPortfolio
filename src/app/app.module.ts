ce479bd5ea90dbfe76679401a1f4136c9e9c6bd9
ギッチ <34914112+Gitschi@users.noreply.github.com>
2018-10-08 17:41:56 +0900

    Create Landing Page Basic
    - Created Basic Design for Landing Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
